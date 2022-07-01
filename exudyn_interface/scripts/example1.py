aedb00b4380016f608be73732ed42ca7f81aff24
Aaron Bacher <aaronbacher@gmx.de>
2022-07-01 16:09:56 +0200

    improved comments

--- exudyn_interface/scripts/example1.py
+++ exudyn_interface/scripts/example1.py
@@ -5,7 +5,7 @@ Author: Aaron Bacher
 Date: 20.06.2022
 
 Example-script for testing interface between exudyn and robot-controller
-Implements single pendulum moving in yz-plane
+Implements single pendulum moving in yz-plane (of robot base coordinate system)
 """
 
 import exudyn as exu
@@ -71,26 +71,26 @@ def main():
     rospy.init_node('ExudynExample1', anonymous=True)
     pub = rospy.Publisher('/my_cartesian_impedance_controller/setDesiredPose', PoseStamped, queue_size=1000)
 
-    # erstelle Systemcontainer und
+    # init exudyn
     SC = exu.SystemContainer()
     mbs = SC.AddSystem()
 
-    tRes = 0.001 # step size
-    tEnd = 3
+    tRes = 0.001 # step size in s
+    tEnd = 3     # simulation time in s
 
-    g = 9.81    # gravity
+    g = 9.81    # gravity in m/s^2
 
-    # start position of robot in robot frame in meters
+    # start position of robot in robot base frame in meters
     globalStartPos = np.array([0.3, 0, 0.2])
 
-    # align mbs with robot axis
-    rotMat = np.array([[0, 0, 1],
-                       [0, 1, 0],
-                       [1, 0, 0]])
+    # matrix to transform simulation coordinates to robot world coordinates
+    trafoMat = np.array([[0, 0, 1],
+                         [0, 1, 0],
+                         [1, 0, 0]])
 
     #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    # parameter und ground mit Rechteck Visualisierung
-    #rigid pendulum:
+    # parameter and ground with rectangle visualisation
+    # rigid pendulum:
     rect = [-2,-2,2,2] #xmin,ymin,xmax,ymax
     background = {'type':'Line', 'color':[0.1,0.1,0.8,1], 'data':[rect[0],rect[1],0, rect[2],rect[1],0, rect[2],rect[3],0, rect[0],rect[3],0, rect[0],rect[1],0]} #background
     oGround=mbs.AddObject(ObjectGround(referencePosition= [0,0,0], visualization=VObjectGround(graphicsData= [background])))
@@ -99,102 +99,107 @@ def main():
     massRigid = 12
     inertiaRigid = massRigid/12*(2*a)**2
 
-    # Körper bestehend aus Rigid2D Node (Koordinaten) und Objekt RigidBody2D (physikal. Eigenschaften und Visualisierung)
+    # body consists out of Rigid2D Node (coordinates) and objekt RigidBody2D (physical properties and visualisation)
     graphics2 = {'type':'Line', 'color':[0.1,0.1,0.8,1], 'data':[-a,-b,0, a,-b,0, a,b,0, -a,b,0, -a,-b,0]} #background
     nRigid = mbs.AddNode(Rigid2D(referenceCoordinates=[-1,0.5,-np.pi/2], initialVelocities=[0,0,5]));
     oRigid = mbs.AddObject(RigidBody2D(physicsMass=massRigid, physicsInertia=inertiaRigid,nodeNumber=nRigid,visualization=VObjectRigidBody2D(graphicsData= [graphics2])))
 
-    # Erstellen der Marker:
-    # mR1 ist das obere Ende des körpers und der Punkt an dem das Gelenk angebracht wird
-    # mR2 ist im Mittelpunkt des Körpers
-    # mR3 ist am unteren Ende des Pendelkörpers
-    # mG0 ist starr mi dem Ground verbunden
-    # durch ein RevoluteJoint2D werden die Marker mG0 und und mR1 verbunden -> nur Verdrehung zueinander möglich
-    mR1 = mbs.AddMarker(MarkerBodyPosition(bodyNumber=oRigid, localPosition=[-0.5,0.,0.])) #support point
-    mR2 = mbs.AddMarker(MarkerBodyPosition(bodyNumber=oRigid, localPosition=[ 0.,0.,0.])) #mid point
-    mR3 = mbs.AddMarker(MarkerBodyPosition(bodyNumber=oRigid, localPosition=[ 0.5,0.,0.])) #end point
+    # create markers:
+    # mG0 lays on the ground where the pendulum is connected
     mG0 = mbs.AddMarker(MarkerBodyPosition(bodyNumber=oGround, localPosition=[-1,1.,0.]))
+
+    # mR1 lays on the upper end of the pendulum where it is connected with ground
+    mR1 = mbs.AddMarker(MarkerBodyPosition(bodyNumber=oRigid, localPosition=[-0.5,0.,0.]))
+
+    # mR2 lays in the middle of the pendulum where the gravitational force applies
+    mR2 = mbs.AddMarker(MarkerBodyPosition(bodyNumber=oRigid, localPosition=[ 0.,0.,0.]))
+
+    # mR3 lays on the lower end of the pendulum where the user can interact
+    mR3 = mbs.AddMarker(MarkerBodyPosition(bodyNumber=oRigid, localPosition=[ 0.5,0.,0.]))
+
+    # a RevoluteJoint2D allows only rotation between markers mG0 and mR1
     mbs.AddObject(RevoluteJoint2D(markerNumbers=[mG0,mR1]))
 
-    # Erstelle Gravitationskraft auf den Körper
-    mbs.AddLoad(Force(markerNumber=mR2, loadVector=[0, -massRigid*g, 0]))   # Gravity
+    # gravitational force for pendulum
+    mbs.AddLoad(Force(markerNumber=mR2, loadVector=[0, -massRigid*g, 0]))
 
-    # Positionssensor für Pendel-Endpunkt
+    # sensor for position of endpoint of pendulum
     sensorPos = mbs.AddSensor(SensorMarker(markerNumber=mR3,
                                            outputVariableType=exu.OutputVariableType.Position))
+    # store sensor value of each step in mbs variable, so that is accessible from user function
     mbs.variables['pos'] = sensorPos
 
-    posOffset = np.array([0, 0, 0])
-    i = 0
-    T = np.eye(4)    # full coordinate transformation
+    # initialisation of some variables
+    posOffset = np.array([0, 0, 0]) # offset between user-interact position in simulation and robot world space 
+    firstPose = True                # flag to determine first step; needed to to calculate posOffset
+    T = np.eye(4)                   # for full coordinate transformation
 
     def PreStepUserFunction(mbs, t):
-        nonlocal i
+        nonlocal firstPose
         nonlocal posOffset
         nonlocal T
 
+        # read current position
         pos_ = mbs.GetSensorValues(mbs.variables['pos'])
         pos = np.array(pos_)
 
-        if i == 0:
+        # in first iteration, calculate posOffset and T
+        if firstPose:
             buf = [pos[2], pos[0], pos[1]]
             posOffset = globalStartPos - buf
             # full coordinate transformation
             posOffset = np.expand_dims(posOffset, axis=1)
-            #T = np.concatenate((rotMat, posOffset), axis=1)
+            #T = np.concatenate((trafoMat, posOffset), axis=1)
             #T = np.concatenate((T, np.array([[0, 0, 0, 1]])), axis=0)
+            firstPose = False
 
-
+        # initilaize container
         posGlobal = [0, 0, 0]
+
+        # transform simulation coordinates to robot world coordinates
+        #pos = np.array([pos[0], pos[1], pos[2], 1])
+        #posGlobal = T @ pos
+        #posGlobal = posGlobal[:3]
         
+        # as for now no rotation is required, it is faster to compute coordinates without matrix multiplication
         posGlobal[0] = pos[2] + posOffset[0][0]
         posGlobal[1] = pos[0] + posOffset[1][0]
         posGlobal[2] = pos[1] + posOffset[2][0]
 
-        #print(i, posGlobal, t)
-        i += 1
-
+        # compose message and publish
         msg = createPoseStampedMsg(posGlobal, (180, 0, 0))
         pub.publish(msg)
 
+        # prestep-userfunction has to return true, else simulation stops
         return True
 
     mbs.SetPreStepUserFunction(PreStepUserFunction)
 
-    # Zuweisen der Anfangswerte, verbinden der Komponenten, Checks
+    # assemble multi body system with all previous specified properties and components
     mbs.Assemble()
 
-    # Setzen der Simulationseinstellungen: Simulationsdauer, Toleranzen, Löser, Informationen über Lösung
+    # set simulation settings
     simulationSettings = exu.SimulationSettings() #takes currently set values or default values
     simulationSettings.timeIntegration.endTime = tEnd
-    simulationSettings.timeIntegration.numberOfSteps = int(tEnd/tRes)  # Anzahl Schritte
-    simulationSettings.timeIntegration.newton.relativeTolerance = 1e-8*100 #10000
+    simulationSettings.timeIntegration.numberOfSteps = int(tEnd/tRes)
+    simulationSettings.timeIntegration.newton.relativeTolerance = 1e-8*100
     simulationSettings.timeIntegration.newton.absoluteTolerance = 1e-10
-    simulationSettings.timeIntegration.verboseMode = 1 # wenn 0 kein Output; höher --> mehr Output informationen über Solver
+    simulationSettings.timeIntegration.verboseMode = 1 # if 0 no output; higher --> more output information about solver
     simulationSettings.timeIntegration.newton.useModifiedNewton = False
     simulationSettings.timeIntegration.newton.numericalDifferentiation.minimumCoordinateSize = 1
-    simulationSettings.timeIntegration.generalizedAlpha.spectralRadius = 0.5 # Spektralradius des Lösers --> Siehe L
+    simulationSettings.timeIntegration.generalizedAlpha.spectralRadius = 0.5
     simulationSettings.timeIntegration.simulateInRealtime = True    # crucial for operating with robot
     simulationSettings.displayStatistics = True
-    exu.showHints = True
+    simulationSettings.solutionSettings.solutionInformation = "2D Pendulum"
 
-
-    simulationSettings.solutionSettings.solutionInformation = "2D Pendel"
-
-    # Starte Renderer
+    # exudyn magic
     exu.StartRenderer()
-
-    # warte auf Userinput (Space)
     mbs.WaitForUserToContinue() # space/q to start - q to end
-
-    # Löse das System
     exu.SolveDynamic(mbs, simulationSettings)
-
-    # schließe Visualisierung nach dem StopFlag (q)
     #SC.WaitForRenderEngineStopFlag()
     exu.StopRenderer() #safely close rendering window!
 
-    #print(mbs.variables['sensorRecord'+str(sensorPos)])
+    # remove "coordinatesSolution.txt" as it isn't needed
     import os
     os.remove("coordinatesSolution.txt")
 
