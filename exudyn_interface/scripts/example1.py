14d02f5065c46956345e9db69723a8cf90439730
Aaron Bacher <aaron.bacher@student.uibk.ac.at>
2022-07-26 12:10:26 +0200

    improved formatting

--- exudyn_interface/scripts/example1.py
+++ exudyn_interface/scripts/example1.py
@@ -144,18 +144,30 @@ def main():
     #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # parameter and ground with rectangle visualisation
     # rigid pendulum:
-    rect = [-2,-2,2,2] #xmin,ymin,xmax,ymax
-    background = {'type':'Line', 'color':[0.1,0.1,0.8,1], 'data':[rect[0],rect[1],0, rect[2],rect[1],0, rect[2],rect[3],0, rect[0],rect[3],0, rect[0],rect[1],0]} #background
-    oGround=mbs.AddObject(ObjectGround(referencePosition= [0,0,0], visualization=VObjectGround(graphicsData= [background])))
-    a = 0.5     #x-dim of pendulum
-    b = 0.05    #y-dim of pendulum
+    rect = [-2, -2, 2, 2]   # xmin, ymin, xmax, ymax
+    background = {'type': 'Line', 'color': [0.1, 0.1, 0.8, 1], 'data': [rect[0], rect[1], 0,
+                                                                        rect[2], rect[1], 0,
+                                                                        rect[2], rect[3], 0,
+                                                                        rect[0], rect[3], 0,
+                                                                        rect[0], rect[1], 0]}   # background
+    oGround = mbs.AddObject(ObjectGround(referencePosition=[0, 0, 0],
+                                         visualization=VObjectGround(graphicsData=[background])))
+    a = 0.5     # x-dim of pendulum
+    b = 0.05    # y-dim of pendulum
     massRigid = 12
     inertiaRigid = massRigid/12*(2*a)**2
 
     # body consists out of Rigid2D Node (coordinates) and objekt RigidBody2D (physical properties and visualisation)
-    graphics2 = {'type':'Line', 'color':[0.1,0.1,0.8,1], 'data':[-a,-b,0, a,-b,0, a,b,0, -a,b,0, -a,-b,0]} #background
-    nRigid = mbs.AddNode(Rigid2D(referenceCoordinates=[-1,0.5,-np.pi/2], initialVelocities=[0,0,0]));
-    oRigid = mbs.AddObject(RigidBody2D(physicsMass=massRigid, physicsInertia=inertiaRigid,nodeNumber=nRigid,visualization=VObjectRigidBody2D(graphicsData= [graphics2])))
+    graphics2 = {'type':'Line', 'color':[0.1, 0.1, 0.8, 1], 'data':[-a, -b, 0,
+                                                                     a, -b, 0,
+                                                                     a,  b, 0,
+                                                                    -a,  b, 0,
+                                                                    -a, -b, 0]}     # background
+    nRigid = mbs.AddNode(Rigid2D(referenceCoordinates=[-1, 0.5, -np.pi/2], initialVelocities=[0, 0, 0]))
+    oRigid = mbs.AddObject(RigidBody2D(physicsMass=massRigid,
+                                       physicsInertia=inertiaRigid,
+                                       nodeNumber=nRigid,
+                                       visualization=VObjectRigidBody2D(graphicsData=[graphics2])))
 
     # create markers:
     # mG0 lies on the ground where the pendulum is connected
@@ -171,7 +183,7 @@ def main():
     mR3 = mbs.AddMarker(MarkerBodyPosition(bodyNumber=oRigid, localPosition=[0.5, 0., 0.]))
 
     # a RevoluteJoint2D allows only rotation between markers mG0 and mR1
-    mbs.AddObject(RevoluteJoint2D(markerNumbers=[mG0,mR1]))
+    mbs.AddObject(RevoluteJoint2D(markerNumbers=[mG0, mR1]))
 
     # gravitational force for pendulum
     mbs.AddLoad(Force(markerNumber=mR2, loadVector=[0, -massRigid*g, 0]))
@@ -185,12 +197,6 @@ def main():
     mFz = mbs.AddMarker(MarkerNodeCoordinate(nodeNumber=nRigid, coordinate=2))
     mbs.AddLoad(LoadCoordinate(markerNumber=mFx,
                                loadUserFunction=UFloadX))
-
-    #extEfforts[0:3] = np.array([[3,0,0]]).T # testing
-    #mbs.AddLoad(Force(markerNumber=mR3, loadVector=extEfforts[0:3]))    # TODO does this update when changing extEfforts?
-
-    # external applied moments
-    #mbs.AddLoad(Torque(markerNumber=mR3, loadVector=extEfforts[3:6]))   # TODO not sure if correct
 
     # sensor for position of endpoint of pendulum
     sensorPos = mbs.AddSensor(SensorMarker(markerNumber=mR3,
