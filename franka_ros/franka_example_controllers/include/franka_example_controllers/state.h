b45b7180ee4ce376bcd6031fc97494e68925720d
Aaron Bacher <aaron.bacher@student.uibk.ac.at>
2022-10-17 12:50:20 +0200

    improved code readability

--- franka_ros/franka_example_controllers/include/franka_example_controllers/state.h
+++ franka_ros/franka_example_controllers/include/franka_example_controllers/state.h
@@ -12,11 +12,11 @@ struct State{
     double acc = 0.0;
     double jerk = 0.0;
 
-    const State operator+ (State state2);
-    const State operator- (State state2);
-    const State operator* (double factor);
-    double& operator[] (int i);
-    const bool operator==(State state2);
+    const State operator+ (State state2);   // addition
+    const State operator- (State state2);   // subtraction
+    const State operator* (double factor);  // multiplication
+    double& operator[] (int i);             // access pos, vel, acc, jerk with indices
+    const bool operator==(State state2);    // comparison, equality check
 };
 
 // struct representing kinematic state for three dimensions
@@ -25,11 +25,11 @@ struct State3{
     State y;
     State z;
 
-    const State3 operator+ (State3 state2);
-    const State3 operator- (State3 state2);
-    const State3 operator* (double factor);
-    State& operator[] (int i);
-    const bool operator==(State3 state2);
+    const State3 operator+ (State3 state2); // addition
+    const State3 operator- (State3 state2); // subtraction
+    const State3 operator* (double factor); // multiplication
+    State& operator[] (int i);              // access x, y, z with indices
+    const bool operator==(State3 state2);   // comparison, equality check
 };
 
 #endif //SRC_STATE_H
--- franka_ros/franka_example_controllers/src/state.cpp
+++ franka_ros/franka_example_controllers/src/state.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 
 /*
- *  Implementations for Methods of State
+ *  Implementations for State-methods
  */
 
 const State State::operator+ (State state2){
@@ -19,21 +19,21 @@ const State State::operator+ (State state2){
     return res;
 }
 
-const State State::operator* (double factor){
-    State res;
-    res.pos = pos * factor;
-    res.vel = vel * factor;
-    res.acc = acc * factor;
-    res.jerk = jerk * factor;
-    return res;
-}
-
 const State State::operator- (State state2){
     State res;
     res.pos = pos - state2.pos;
     res.vel = vel - state2.vel;
     res.acc = acc - state2.acc;
     res.jerk = jerk - state2.jerk;
+    return res;
+}
+
+const State State::operator* (double factor){
+    State res;
+    res.pos = pos * factor;
+    res.vel = vel * factor;
+    res.acc = acc * factor;
+    res.jerk = jerk * factor;
     return res;
 }
 
@@ -53,7 +53,7 @@ const bool State::operator==(State state2){
 }
 
 /*
- *  Implementations for Methods of State3
+ *  Implementations for State3-methods
  */
 
 const State3 State3::operator+ (State3 state2){
@@ -64,19 +64,19 @@ const State3 State3::operator+ (State3 state2){
     return res;
 }
 
-const State3 State3::operator* (double factor){
-    State3 res;
-    res.x = x * factor;
-    res.y = y * factor;
-    res.z = z * factor;
-    return res;
-}
-
 const State3 State3::operator- (State3 state2){
     State3 res;
     res.x = x - state2.x;
     res.y = y - state2.y;
     res.z = z - state2.z;
+    return res;
+}
+
+const State3 State3::operator* (double factor){
+    State3 res;
+    res.x = x * factor;
+    res.y = y * factor;
+    res.z = z * factor;
     return res;
 }
 
