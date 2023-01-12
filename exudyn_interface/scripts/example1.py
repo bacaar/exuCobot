4b1e6ea9bdeaf9cec22d022f2d34d5b4d3958660
Aaron Bacher <aaron.bacher@student.uibk.ac.at>
2023-01-12 13:46:45 +0100

    increased force threshold

--- exudyn_interface/scripts/example1.py
+++ exudyn_interface/scripts/example1.py
@@ -95,7 +95,7 @@ def externalForceCallback(data):
 
     # additional to force offset, there is also some noise on force/torque measurement
     # thus neglect everything which is smaller than 0.5N
-    threshold = 1
+    threshold = 3
 
     if abs(fy) >= threshold:
         extEfforts[0] = fy
